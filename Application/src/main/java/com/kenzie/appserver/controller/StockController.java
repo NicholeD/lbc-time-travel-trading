4dd4aaf099b2359befec0043ebcaeb6edbf75c7d
NicholeD <YOUREMAIL@YOUREMAIL.com>
2022-09-21 20:57:53 -0600

    Refactored Seach classes to Stock classes, implemented StockResponse and StockController.

--- Application/src/main/java/com/kenzie/appserver/controller/StockController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/StockController.java
@@ -1,10 +1,51 @@
 package com.kenzie.appserver.controller;
 
+import com.kenzie.appserver.controller.model.StockResponse;
+import com.kenzie.appserver.repositories.StockRepository;
+import com.kenzie.appserver.service.StockService;
+import com.kenzie.appserver.service.model.Stock;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/symbol")
 public class StockController {
 
+    private StockService stockService;
+
+    StockController(StockService stockService) { this.stockService = stockService; }
+
+    @GetMapping("/{symbol}")
+    public ResponseEntity<List<StockResponse>> searchStocksBySymbol(@PathVariable("symbol") String symbol) {
+        List<Stock> stocks = stockService.findBySymbol(symbol);
+
+        if (stocks == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        List<StockResponse> stockResponses = createStockResponse(stocks);
+        return ResponseEntity.ok(stockResponses);
+    }
+
+
+    private List<StockResponse> createStockResponse(List<Stock> stocks) {
+        List<StockResponse> stockResponses = new ArrayList<>();
+        for (Stock stock : stocks) {
+            StockResponse stockResponse = new StockResponse();
+            stockResponse.setSymbol(stock.getSymbol());
+            stockResponse.setName((stock.getName()));
+            stockResponse.setPurchasePrice(stock.getPurchasePrice());
+            stockResponse.setPurchaseDate(stock.getPurchaseDate());
+
+            stockResponses.add(stockResponse);
+        }
+
+        return stockResponses;
+    }
 }
---
+++ Application/src/main/java/com/kenzie/appserver/controller/model/StockResponse.java
@@ -0,0 +1,54 @@
+package com.kenzie.appserver.controller.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.ZonedDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StockResponse {
+
+    @JsonProperty("symbol")
+    private String symbol;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("purchasePrice")
+    private double purchasePrice;
+
+    @JsonProperty("purchaseDate")
+    private ZonedDateTime purchaseDate;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public ZonedDateTime getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(ZonedDateTime purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+}
